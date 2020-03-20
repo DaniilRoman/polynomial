6f1a3bd496d21c0acfcd5a884fd74a6c35545abc
Daniil Roman <danroman17397@gmail.com>
2020-03-21 00:36:10 +0300

    [hw 2] add dynamic programing algorithm

--- StockMarket/stock_market.py
+++ StockMarket/stock_market.py
@@ -7,8 +7,8 @@ class Item:
         self.name = name
         self.price = price
         self.count = count
-        self.totalCost = price * 10 * count
-        self.totalReward = (1000 * count + (day + 30) * self.count) - self.totalCost
+        self.total_cost = int(price * 10 * count)
+        self.total_reward = (1000 * count + (day + 30) * self.count) - self.total_cost
 
 
 class Investor:
@@ -16,17 +16,21 @@ class Investor:
         self.n_days = n_days
         self.m_items = m_items
         self.s_money = s_money
+
         self.items = []
+        self.total_reward = None
 
     def add_item(self, item):
-        if self.s_money - item.totalCost >= 0:
+        if self.s_money - item.total_cost >= 0:
             self.items.append(item)
-            self.s_money -= item.totalCost
+            self.s_money -= item.total_cost
 
     def get_total_reward(self):
+        if self.total_reward is not None:
+            return self.total_reward
         total_reward = 0
         for item in self.items:
-            total_reward += item.totalReward
+            total_reward += item.total_reward
         return total_reward
 
 
@@ -40,16 +44,61 @@ class GreedyAlgorithm:
             max_value = 0
             max_item = None
             for item in self.items:
-                if item.totalReward > max_value:
-                    max_value = item.totalReward
+                if item.total_reward > max_value:
+                    max_value = item.total_reward
                     max_item = item
             self.investor.add_item(max_item)
             self.items.remove(max_item)
 
 
-class SolverStrategy:
+class DynamicProgramingAlgorithm:
     def __init__(self, investor, items):
-        self.alg = GreedyAlgorithm(investor, items)
+        self.investor = investor
+        self.items = items
+
+    def calculate(self):
+        def knapsack(W, wt, val, n):
+            K = [[0 for x in range(W + 1)] for x in range(n + 1)]
+
+            for i in range(n + 1):
+                for w in range(W + 1):
+                    if i == 0 or w == 0:
+                        K[i][w] = 0
+                    elif wt[i - 1] <= w:
+                        K[i][w] = max(val[i - 1] + K[i - 1][w - wt[i - 1]],
+                                      K[i - 1][w])
+                    else:
+                        K[i][w] = K[i - 1][w]
+            # K[n][W]
+            return K
+
+        def find_ans(k, s):
+            if _best_reword_matrix[k][s] == 0:
+                return
+            if _best_reword_matrix[k - 1][s] == _best_reword_matrix[k][s]:
+                find_ans(k - 1, s)
+            else:
+                find_ans(k - 1, s - total_cost[k])
+                _answer_indexes.append(k)
+
+        total_reward = [item.total_reward for item in self.items]
+        total_cost = [item.total_cost for item in self.items]
+        investor_money = self.investor.s_money
+        count_papers = len(total_cost)
+
+        _best_reword_matrix = knapsack(investor_money, total_cost, total_reward, count_papers)
+        _answer_indexes = []
+
+        find_ans(count_papers, investor_money)
+
+        self.investor.total_reward = _best_reword_matrix[count_papers][investor_money]
+        for i in _answer_indexes[-1::-1]:
+            self.investor.items.append(self.items[i - 1])
+
+
+class SolverStrategy:
+    def __init__(self, alg):
+        self.alg = alg
 
     def calculate(self):
         self.alg.calculate()
@@ -93,7 +142,8 @@ class ResultWriter:
         result_str += '\n'
         return result_str
 
+
 def calculate_wrapper(file_path):
     investor, items = Parser.parse(file_path)
-    solver = SolverStrategy(investor, items)
+    solver = SolverStrategy(GreedyAlgorithm(investor, items))
     ResultWriter.write(solver)
--- StockMarket/unit_test.py
+++ StockMarket/unit_test.py
@@ -2,12 +2,11 @@ import unittest
 import os
 from statistics import mean
 
-from stock_market import Investor, SolverStrategy, Item, Parser, ResultWriter
+from stock_market import Investor, SolverStrategy, Item, Parser, \
+    ResultWriter, calculate_wrapper, GreedyAlgorithm, DynamicProgramingAlgorithm
 from stock_market_optim import calculate
 
 import time
-
-from StockMarket.stock_market import calculate_wrapper
 
 
 class StockMarketTest(unittest.TestCase):
@@ -15,7 +14,7 @@ class StockMarketTest(unittest.TestCase):
     def test_with_constructing(self):
         items = [Item(3, '1', 80, 2), Item(2, '2.1', 70, 3), Item(2, '2.2', 90, 4)]
         investor = Investor(4, 2, 3800)
-        solver = SolverStrategy(investor, items)
+        solver = SolverStrategy(GreedyAlgorithm(investor, items))
 
         self.assertEqual(
             1462,
@@ -23,15 +22,21 @@ class StockMarketTest(unittest.TestCase):
 
     def test_with_parsing(self):
         investor, items = Parser.parse(os.path.join('test_files', 'first_test.txt'))
-        solver = SolverStrategy(investor, items)
+        solver = SolverStrategy(GreedyAlgorithm(investor, items))
 
         self.assertEqual(
             1462,
             solver.calculate())
 
-    def test_with_writing_result(self):
+    def test_with_writing_result_greedy(self):
+        self._run_with_writing_result(GreedyAlgorithm)
+
+    def test_with_writing_result_dynamic_programing(self):
+        self._run_with_writing_result(DynamicProgramingAlgorithm)
+
+    def _run_with_writing_result(self, alg):
         investor, items = Parser.parse(os.path.join('test_files', 'first_test.txt'))
-        solver = SolverStrategy(investor, items)
+        solver = SolverStrategy(alg(investor, items))
         ResultWriter.write(solver)
 
         actual = Parser.get_text(os.path.join('result_files', 'test_result_actual.txt'))
@@ -69,6 +74,47 @@ class StockMarketTest(unittest.TestCase):
 
         print("--- %s seconds ---" % mean(time_1))
         print("--- %s seconds ---" % mean(time_2))
+
+
+    # def test_gfg_imlpementation(self):
+    #
+    #     def knapsack(W, wt, val, n):
+    #         K = [[0 for x in range(W + 1)] for x in range(n + 1)]
+    #
+    #         for i in range(n + 1):
+    #             for w in range(W + 1):
+    #                 if i == 0 or w == 0:
+    #                     K[i][w] = 0
+    #                 elif wt[i - 1] <= w:
+    #                     K[i][w] = max(val[i - 1] + K[i - 1][w - wt[i - 1]], K[i - 1][w])
+    #                 else:
+    #                     K[i][w] = K[i - 1][w]
+    #         # K[n][W]
+    #         return K
+    #
+    #     def find_ans(k, s):
+    #         if _best_reword_matrix[k][s] == 0:
+    #             return
+    #         if _best_reword_matrix[k - 1][s] == _best_reword_matrix[k][s]:
+    #             find_ans(k - 1, s)
+    #         else:
+    #             find_ans(k - 1, s - total_cost[k])
+    #             _answer_indexes.append(k)
+    #
+    #     total_reward = [60, 100, 120, 500, 200, 15]  # to MAX
+    #     total_cost = [2, 3, 3, 4, 5, 4]  # <= investor_money
+    #     investor_money = 10
+    #     count_papers = len(total_cost)
+    #
+    #     _best_reword_matrix = knapsack(investor_money, total_cost, total_reward, count_papers)
+    #     _answer_indexes = []
+    #
+    #     find_ans(count_papers, investor_money)
+    #     best_reword = _best_reword_matrix[count_papers][investor_money]
+    #
+    #     print(best_reword)
+    #     print(_answer_indexes)
+
 
 if __name__ == '__main__':
     unittest.main()
